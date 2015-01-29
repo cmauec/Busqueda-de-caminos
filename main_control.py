d0f7dc8a781b13595c95adcb27c6116d4132864f
gina1990 <gibeth_1789@yahoo.com.ar>
2015-01-30 00:43:00 +0300

    Robot intenta ceder el paso a otro robot
    No funcuina bien el codigo de colisiones

--- main_control.py
+++ main_control.py
@@ -115,24 +115,67 @@ class Client(object):
             # Dibujamos todos los pedidos pendientes de entrega
             self.control.dibujarPedidos(self.ui.screen)
             if self.robots[0].rec_colision.colliderect(self.robots[1].rec_colision):
-                print 'los robots se chocaron'                   
-                self.robots[0].play_animation = False
-                self.robots[1].play_animation = False
+                print 'los robots se chocaron'  
+                print self.robots[0].posicion_actual
+                print self.robots[1].posicion_actual                  
+                self.robots[0].play_animation = False                
+                index_posicion_actual = self.robots[0].path.index(self.robots[0].posicion_actual)
+                punto_anterior_robot = self.robots[0].path[index_posicion_actual - 1]
+                if self.robots[0].posicion_actual[1] > punto_anterior_robot[1]:
+                    print 'robot desde arriba'
+                    self.robots[0].posicion_actual = (self.robots[0].posicion_actual[0], self.robots[0].posicion_actual[1] - 1)
+                elif self.robots[0].posicion_actual[1] <punto_anterior_robot[1]: 
+                    print 'robot desde abajo'
+                    self.robots[0].posicion_actual = (self.robots[0].posicion_actual[0], self.robots[0].posicion_actual[1] + 1)                
+
                 if self.robots[0].posicion_actual[0] in robot_move_right_wall:
                     self.robots[0].posicion_actual = (self.robots[0].posicion_actual[0]+1, self.robots[0].posicion_actual[1] )
 
                 elif self.robots[0].posicion_actual[0] in robot_move_left_wall:
                     self.robots[0].posicion_actual = (self.robots[0].posicion_actual[0]-1, self.robots[0].posicion_actual[1] )
 
-                elif self.robots[0].posicion_actual[1] in robot_move_down_wall:
+                elif self.robots[0].posicion_actual[1] == robot_move_down_wall:
                     self.robots[0].posicion_actual = (self.robots[0].posicion_actual[0], self.robots[0].posicion_actual[1] +1)
 
-                elif self.robots[0].posicion_actual[1] in robot_move_up_wall:
+                elif self.robots[0].posicion_actual[1] == robot_move_up_wall:
                     self.robots[0].posicion_actual = (self.robots[0].posicion_actual[0], self.robots[0].posicion_actual[1] -1)
+                self.robots[0].play_animation = True
 
                 x, y = self.robots[0].posicion_actual
                 nx, ny = x * NODE_SIZE, y * NODE_SIZE
                 self.robots[0].rec_colision = pygame.Rect(nx, ny, NODE_SIZE, NODE_SIZE)
+
+
+            if self.robots[1].rec_colision.colliderect(self.robots[0].rec_colision):
+                print 'los robots se chocaron'  
+                print self.robots[0].posicion_actual
+                print self.robots[1].posicion_actual                  
+                self.robots[1].play_animation = False                
+                index_posicion_actual = self.robots[1].path.index(self.robots[1].posicion_actual)
+                punto_anterior_robot = self.robots[1].path[index_posicion_actual - 1]
+                if self.robots[1].posicion_actual[1] > punto_anterior_robot[1]:
+                    print 'robot desde arriba'
+                    self.robots[1].posicion_actual = (self.robots[1].posicion_actual[0], self.robots[1].posicion_actual[1] - 1)
+                elif self.robots[1].posicion_actual[1] <punto_anterior_robot[1]: 
+                    print 'robot desde abajo'
+                    self.robots[1].posicion_actual = (self.robots[1].posicion_actual[0], self.robots[1].posicion_actual[1] + 1)                
+
+                if self.robots[1].posicion_actual[0] in robot_move_right_wall:
+                    self.robots[1].posicion_actual = (self.robots[1].posicion_actual[0]+1, self.robots[1].posicion_actual[1] )
+
+                elif self.robots[1].posicion_actual[0] in robot_move_left_wall:
+                    self.robots[1].posicion_actual = (self.robots[1].posicion_actual[0]-1, self.robots[1].posicion_actual[1] )
+
+                elif self.robots[1].posicion_actual[1] == robot_move_down_wall:
+                    self.robots[1].posicion_actual = (self.robots[1].posicion_actual[0], self.robots[1].posicion_actual[1] +1)
+
+                elif self.robots[1].posicion_actual[1] == robot_move_up_wall:
+                    self.robots[1].posicion_actual = (self.robots[1].posicion_actual[0], self.robots[1].posicion_actual[1] -1)
+                self.robots[1].play_animation = True
+
+                x, y = self.robots[1].posicion_actual
+                nx, ny = x * NODE_SIZE, y * NODE_SIZE
+                self.robots[1].rec_colision = pygame.Rect(nx, ny, NODE_SIZE, NODE_SIZE)
                 
 
 
